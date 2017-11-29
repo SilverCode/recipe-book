200a6cf8a7c72d53e0045f6545a2c9f19f1ec717
Ross Simpson <ross@onedigitalmedia.com>
2017-11-29 14:40:22 +0200

    Add Recipe Detail Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
+import { RecipesDetailComponent } from './recipes/recipes-detail/recipes-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     RecipesComponent,
-    RecipesListComponent
+    RecipesListComponent,
+    RecipesDetailComponent
   ],
   imports: [
     BrowserModule,
