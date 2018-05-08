cf045cf2c71207360c7a8e3c19531d86406a3a46
Ross Simpson <ross@onedigitalmedia.com>
2018-05-08 16:18:37 +0200

    Move recipes to RecipeService

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+import {RecipeService} from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -10,20 +11,14 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected: EventEmitter<Recipe> = new EventEmitter<Recipe>();
 
-  recipes: Recipe[] = [
-    new Recipe(
-      'A Test Recipe',
-      'This is a simply a test',
-      'http://maxpixel.freegreatpicture.com/static/photo/1x/Food-Dishes-Eat-Gastronomy-Lunch-Recipe-2760200.jpg'),
-    new Recipe(
-      'A Test Recipe 2',
-      'This is a simply another test',
-      'http://maxpixel.freegreatpicture.com/static/photo/1x/Pumpkin-Seed-Bowl-Seeds-Recipe-Food-Strawberries-1715284.jpg')
-  ];
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(
+    private recipeService: RecipeService
+  ) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(recipe: Recipe) {
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,18 @@
+import {Recipe} from './recipe.model';
+
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is a simply a test',
+      'http://maxpixel.freegreatpicture.com/static/photo/1x/Food-Dishes-Eat-Gastronomy-Lunch-Recipe-2760200.jpg'),
+    new Recipe(
+      'A Test Recipe 2',
+      'This is a simply another test',
+      'http://maxpixel.freegreatpicture.com/static/photo/1x/Pumpkin-Seed-Bowl-Seeds-Recipe-Food-Strawberries-1715284.jpg')
+  ];
+
+  getRecipes(): Recipe[] {
+    return this.recipes.slice();
+  }
+}
