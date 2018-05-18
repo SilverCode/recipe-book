8a366c39b7ac8e19482fcc581d91d57f56b39cd2
Ross Simpson <simpson.ross@gmail.com>
2018-05-18 20:13:41 +0200

    Use routing for navigating between components

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import {NgModel} from '@angular/forms';
+import {RouterModule, Routes} from '@angular/router';
+import {ShoppingListComponent} from './shopping-list/shopping-list.component';
+import {RecipesComponent} from './recipes/recipes.component';
+import {NgModule} from '@angular/core';
+import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
+import {RecipeHomeComponent} from './recipes/recipe-home/recipe-home.component';
+import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+  { path: 'recipes', component: RecipesComponent, children: [
+      { path: '', component: RecipeHomeComponent },
+      { path: 'new', component: RecipeEditComponent },
+      { path: ':id', component: RecipeDetailComponent },
+      { path: ':id/edit', component: RecipeEditComponent }
+    ] },
+  { path: 'shopping-list', component: ShoppingListComponent }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,9 +6,4 @@ import {Component, Output, EventEmitter} from '@angular/core';
   styleUrls: ['header.component.css']
 })
 export class HeaderComponent {
-  @Output() featureSelected: EventEmitter<string> = new EventEmitter<string>();
-
-  onSelect(feature: string) {
-    this.featureSelected.emit(feature);
-  }
 }
