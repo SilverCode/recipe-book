468efa7f845765c91390f88e8c95955d2552c7ce
Ross Simpson <simpson.ross@gmail.com>
2019-01-23 18:18:24 +0200

    Move Shopping List to feature module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,9 @@
-import {NgModel} from '@angular/forms';
 import {RouterModule, Routes} from '@angular/router';
-import {ShoppingListComponent} from './shopping-list/shopping-list.component';
-import {RecipesComponent} from './recipes/recipes.component';
 import {NgModule} from '@angular/core';
-import {RecipeDetailComponent} from './recipes/recipe-detail/recipe-detail.component';
-import {RecipeHomeComponent} from './recipes/recipe-home/recipe-home.component';
-import {RecipeEditComponent} from './recipes/recipe-edit/recipe-edit.component';
+
+import {ShoppingListComponent} from './shopping-list/shopping-list.component';
 import {SignupComponent} from './auth/signup/signup.component';
 import {SigninComponent} from './auth/signin/signin.component';
-import {AuthGuardService} from './auth/auth-guard.service';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
