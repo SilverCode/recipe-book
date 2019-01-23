640e045f4a37cb12fcc0be21c18dde76f4912ca5
Ross Simpson <simpson.ross@gmail.com>
2019-01-23 18:23:36 +0200

    Move Auth into feature module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,30 +8,26 @@ import {ShoppingListService} from './shopping-list/shopping-list.service';
 import {AppRoutingModule} from './app-routing.module';
 import {RecipeService} from './recipes/recipe.service';
 import {DataStorageService} from './shared/data-storage.service';
-import {SignupComponent} from './auth/signup/signup.component';
-import {SigninComponent} from './auth/signin/signin.component';
 import {AuthService} from './auth/auth.service';
 import {AuthGuardService} from './auth/auth-guard.service';
 import {RecipesModule} from './recipes/recipes.module';
 import {SharedModule} from './shared/shared.module';
 import {ShoppingListModule} from './shopping-list/shopping-list.module';
-import {FormsModule} from '@angular/forms';
+import {AuthModule} from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SignupComponent,
-    SigninComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     RecipesModule,
     ShoppingListModule,
+    AuthModule,
     SharedModule,
     AppRoutingModule,
-    FormsModule
   ],
   providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuardService],
   bootstrap: [AppComponent]
